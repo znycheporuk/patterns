53193d1be66c1ce318f258319a22fcfde0546201
znycheporuk <nycheporuk.zakhar@gmail.com>
2023-06-16 18:10:50 +0300

    fix event-source.test.ts

--- src/event-bus/event-source.test.ts
+++ src/event-bus/event-source.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from "bun:test";
+import { describe, expect, it, jest, spyOn } from "bun:test";
 import { EventBus } from "~/event-bus/event-bus.ts";
 import { EventSource } from "~/event-bus/event-source.ts";
 
@@ -9,6 +9,8 @@ describe("EventSource", () => {
 		const eventSource = new EventSource(eventBus);
 		const eventType = "Test Event Type";
 		const eventData = "Test Event Data";
+		// @ts-ignore
+		spyOn(eventBus, "publish").mockImplementation(() => null);
 		eventSource.emitEvent(eventType, eventData);
 		expect(eventBus.publish).toHaveBeenCalledTimes(1);
 	});
